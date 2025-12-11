7ab0978cd2225eaabc8dc67ed1d55f5247cbe2f9
Indrajith M <indrajithm@Mac-110.lan>
2025-12-11 23:04:21 +0530

    [feat]: add header of the portfolio

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,39 @@
+import styled, { ThemeProvider } from "styled-components";
+import { darkTheme } from "./utils/Themes";
+import Navbar from "./components/navbar/Navbar";
+import { BrowserRouter } from "react-router-dom";
+
+const Body = styled.div`
+  background-color: ${({ theme }) => theme.bg};
+  width: 100%;
+  overflow-x: hidden;
+  position: relative;
+`;
+
+const Wrapper = styled.div`
+  padding-bottom: 100px;
+  background: linear-gradient(
+      38.73deg,
+      rgba(204, 0, 187, 0.15) 0%,
+      rgba(201, 32, 184, 0) 50%
+    ),
+    linear-gradient(
+      141.27deg,
+      rgba(0, 70, 209, 0) 50%,
+      rgba(0, 70, 209, 0.15) 100%
+    );
+  width: 100%;
+  clip-path: polygon(0 0, 100% 0, 100% 100%, 30% 98%, 0 100%);
+`;
 
 function App() {
-
   return (
-    <>
-      <div>App</div>  
-    </>
-  )
+    <ThemeProvider theme={darkTheme}>
+      <BrowserRouter>
+        <Navbar />
+      </BrowserRouter>
+    </ThemeProvider>
+  );
 }
 
-export default App
+export default App;
---
+++ src/data/constants.js
@@ -0,0 +1,178 @@
+export const Bio = {
+  name: "Indrajith M",
+  roles: [
+    "Full Stack Developer",
+    "Programmer",
+  ],
+  description:
+    "I'm a highly motivated and versatile individual with a strong aptitude for problem-solving and a deep commitment to continuous development. I enjoy tackling complex challenges, analyzing data, and developing effective solutions. I'm a firm believer in lifelong learning and actively pursue opportunities to expand my knowledge and skillset.",
+  github: "https://github.com/indar12",
+  gmail: "indrajithmohanasundaram@gmail.com",
+  resume:
+    "",
+  linkedin: "https://www.linkedin.com/in/m-indrajith/",
+
+};
+
+export const skills = [
+  {
+    title: "Frontend",
+    skills: [
+      {
+        name: "React Js",
+        image:
+            ""
+      },
+      {
+        name: "Redux Toolkit",
+        image:
+            ""
+    },
+     
+      {
+        name: "HTML",
+        image: ""
+      },
+      {
+        name: "CSS",
+        image:
+            ""
+      },
+      {
+        name: "JavaScript",
+        image:
+            ""
+      },
+      {
+        name: "SCSS",
+        image:
+        ""
+    },
+   
+    ],
+  },
+  {
+    title: "Backend",
+    skills: [
+      {
+        name: "Node Js",
+        image: ""
+      },
+      {
+        name: "Express Js",
+        image:
+            ""
+      },
+      {
+        name: "MySQL",
+        image:
+            ""
+    },
+    ],
+  },
+
+  {
+    title: "Tools",
+    skills: [
+      {
+        name: "Git",
+        image:
+            ""
+    },
+      {
+        name: "GitHub",
+        image:
+            ""
+    },
+      {
+        name: "VS Code",
+        image:
+            ""
+    },
+
+    ],
+  },
+];
+
+export const experiences = [
+  {
+    id: 0,
+    img: "",
+    role: "Senior Project Engineer",
+    company: "Soliton Technologies",
+    date: "June 2025 - Sep 2025",
+    desc: "",
+    skills: [
+      "ReactJS",
+      "Redux",
+      "HTML",
+      "CSS",
+      "JavaScript",
+      "API Integration",
+    ],
+    doc: "",
+  },
+  {
+    id: 1,
+    img: "",
+    role: "Project Engineer",
+    company: "Soliton Technologies",
+    date: "June 2023 - May 2025",
+    desc: "",
+    skills: [
+      "ReactJS",
+      "Redux",
+      "HTML",
+      "CSS",
+      "JavaScript",
+      "API Integration",
+    ],
+    doc: "",
+  },
+  {
+    id: 2,
+    img: "",
+    role: "Intern",
+    company: "Soliton Technologies",
+    date: "July 2022 - May 2023",
+    desc: "",
+    skills: [
+      "ReactJS",
+      "Redux",
+      "HTML",
+      "CSS",
+      "JavaScript",
+      "API Integration",
+    ],
+    doc: "",
+  },
+];
+
+export const education = [
+  {
+    id: 0,
+    img: "",
+    school: "Kongu Engineering College",
+    date: "Aug 2019 - May 2023",
+    grade: "8.52 CGPA",
+    desc: "I have completed 4 years and have a CGPA of 8.52 with First class with distinction.",
+    degree: "Bachelor of Engineering - BE, Computer Science",
+  },
+  {
+    id: 1,
+    img: "",
+    school: "Vidhyaa Vikas Matric Hr. Sec. School",
+    date: "June 2018 - May 2019",
+    grade: "89.5%",
+    desc: "",
+  },
+  {
+    id: 2,
+    img: "",
+    school: "Holy Angels Matric Hr. Sec. School",
+    date: "June 2016 - May 2017",
+    grade: "96%",
+    desc: "",
+  },
+];
+
